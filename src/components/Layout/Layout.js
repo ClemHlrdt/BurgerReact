2cfe3f5285e13cef42a91349bb6e516c4be4348a
Clément Hélardot <clement.helardot@gmail.com>
2018-11-17 17:15:47 +0100

    Added state to the BurgerBuilder component and passed the state as props for the Burger Component. The content show is now dynamic.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from "./Layout.css";
 // Wrapper component
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <div>Toolbar, SideDrawer, Backdrop</div> {/* NavBar*/}
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
