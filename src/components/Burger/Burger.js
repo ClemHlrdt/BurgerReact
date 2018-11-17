6178e108299c962c7e69c8c5f4db5cd1e4cf4993
Clément Hélardot <clement.helardot@gmail.com>
2018-11-17 17:29:23 +0100

    Used reduced higher order function to flatten the array of BurgerIngredient components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,19 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  //Get the keys of the ingredient object
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // For each key, loop...
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // Create a BurgerIngredient component based on the number of element
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
