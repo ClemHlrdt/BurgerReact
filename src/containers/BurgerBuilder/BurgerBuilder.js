a68d0482cf5a8d5bc601f6ce85e20b0348672776
Clément Hélardot <clement.helardot@gmail.com>
2019-02-21 21:23:40 +0100

    Added React routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,28 +95,28 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert("You continue");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Clément",
-        address: "Teststreet 1",
-        zipCode: "12345",
-        country: "Poland",
-        email: "test@test.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-        console.log(error);
-      }); //Firebase endpoint
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Clément",
+    //     address: "Teststreet 1",
+    //     zipCode: "12345",
+    //     country: "Poland",
+    //     email: "test@test.com"
+    //   },
+    //   deliveryMethod: "fastest"
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+    //     console.log(error);
+    //   }); //Firebase endpoint
   };
   render() {
     const disabledInfo = {
